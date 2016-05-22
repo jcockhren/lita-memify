f06ef11a97c4683c8f966b801b52c5ab68b47c38
Jurnell Cockhren <jurnell.cockhren@sophicware.com>
2016-05-22 01:29:05 -0500

    added route method

--- lib/lita/handlers/memify.rb
+++ lib/lita/handlers/memify.rb
@@ -2,10 +2,16 @@ module Lita
   module Handlers
     class Memify < Handler
       route(/^hello/, :do_something, help: {"hello" => "Says hello back"})
-           
-     def do_something(response)
-     	response.reply ("Hello there")
-     end
+      route(/^get random meme/, :get_random_meme, help: {"get random meme" => "finds random meme"})
+
+      def do_something(response)
+     	  response.reply ("Hello there")
+      end
+
+      def get_random_meme(response)
+      end
+
+
       Lita.register_handler(self)
     end
 
